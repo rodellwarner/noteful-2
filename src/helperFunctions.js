315d76da728b51e45962d453f8ae2167eec0c8ad
Rodell Warner <rodell.warner@gmail.com>
2020-09-14 14:21:04 -0500

    deleted obsolote files

--- src/helperFunctions.js
+++
@@ -1,5 +0,0 @@
-export const findFolder = (folders = [], folderId) =>
-  folders.find((folder) => folder.id === folderId);
-
-export const findNote = (notes = [], noteId) =>
-  notes.find((note) => note.id === noteId);
